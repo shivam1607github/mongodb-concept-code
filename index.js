19596086f886560bc000d9939237373a9d6d65c0
shivam1607github <shivampandeysbm1607@gmail.com>
2025-01-03 21:07:50 +0530

    applying hashing by bcrypt & aplying input validation by Zod

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const express = require("express");               //import the express library 
+const bcrypt = require("bcrypt")                       //importing a bcrypt library to hash the password which would be impposibe to decode
+
+const express = require("express");               //import the express library
 const { UserModel , TodoModel } = require("./db");  //connecting the .db file to fetch it
 const jwt = require("jsonwebtoken");      // import the jsonwebtoken
 const JWT_SECRET = "ilovekiyara";    //create a json secret passowrd
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");       //importing the mongoose library for the mongo db concept
+const {z}= require("zod");          //importing the zod library
 mongoose.connect("mongodb+srv://shivampandeysbm1607:shivam1607@cluster0.e7iqg.mongodb.net/todos-app-database");
 const app = express();
 app.use(express.json());           //body parser middleware --
 
+
 app.post("/signup", async function (req,res){
+
+    const requiredbody= z.object({                 //hearing from 16 to 29th line we,re using the zod concept for the input validation 
+        email :z.string(),                         //zod is the library in which its provide a schema through which we can validate our input such as - correct email, min or max password ,the password should be of 8 number and more or less.
+        name :z.string(),
+        password :z.string()
+
+    })
+    const parsedDataWithSuccess = requiredbody.safeParse(req.body);
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message : "incorrect format"
+        })
+        return
+    }
+
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-     // Check if the user already exists
+    const hashedPassword = await bcrypt.hash(password,5);        //called a function bcrypt.hash which will hash the password
+    console.log(hashedPassword);
+
+    // Check if the user already exists
      const existingUser = await UserModel.findOne({ email: email });
      if (existingUser) {
          return res.status(400).json({
@@ -22,7 +44,7 @@ app.post("/signup", async function (req,res){
 
     await UserModel.create({           //inserting the strings by usermodel.insert
         name: name,
-        password : password,
+        password : hashedPassword,
         email : email
     })
 
@@ -35,19 +57,28 @@ app.post("/login", async function (req,res){
     const email = req.body.email;
     const password = req.body.password;
 
-    const user = await UserModel.findOne({
+    const response = await UserModel.findOne({
         email : email,
-        password : password
+        
     })
+    if(!response){
+        res.json({
+            message : "user does not exit in our database"
 
-    console.log(user);
+        })
+        return ;
+    }
 
-    if(user){
+    console.log(response);
+    
+    const passowrdMatch = await bcrypt.compare(password,response.password);
+
+    if(passowrdMatch){
         console.log({
-            id: user.id.toString()
+            id: response.id.toString()
         })
         const token = jwt.sign({
-            id : user._id.toString()
+            id : response._id.toString()
         },JWT_SECRET);
         res.json({
             token : token 
